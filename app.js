d6843367b48206137a50f9d210bb9345bc701371
FelipeAMS <xfoarmks@gmail.com>
2022-09-30 12:41:41 -0400

    Implementado estructura de archivos + ignore node_modules

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3030, () => {
+    console.log("Server in port 3030 online.")
+})
