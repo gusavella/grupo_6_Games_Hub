fe851dc0c67852f9c69336f93c9ab1e928f30d9c
Gonzalo Potente <gonzalopotente2772@gmail.com>
2022-10-16 19:47:04 -0300

    register commit
    register casi terminado, falta consultar el footer

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const path = require('path');
 
 const app = express();
 
+// const publicPath = path.resolve(__dirname, './public');
+
+//app.use(express.static(__dirname + '/public'));
+
 const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+app.use( express.static(publicPath) );
 
 app.listen(3030, () => {
     console.log("Server in port 3030 online.")
