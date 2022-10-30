4c35b1dbcb47379ef56baac5dc917a33477a3953
Gustavo Avella <gusavella1984@gmail.com>
2022-10-30 14:09:32 -0500

    Agrego en carrito visualizacion dnamica de productos

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,8 +1,14 @@
 
-
+let products = require('../models/product')
 const controller = {
   cart: (req, res) => {
-    res.render("productCart",{tittle:'hola'});
+    let totalCart=0
+     for(let i = 0; i<products.length ; i++) { 
+      totalCart=products[i].value+totalCart
+     }
+  
+    res.render("productCart",{products:products,totalCart:totalCart});
+
   },
 
 };
--- models/product.js
+++ models/product.js
@@ -2,23 +2,30 @@ const products = [
     {
         id:1,
         name:'Need for speed heat',
-        price:12345,
+        value:12345,
         discount:25,
         imageUri:'/images/games/need_for_speed_heat.jpg'
     },
     {
         id:2,
         name:'Dota 2',
-        price:12345,
+        value:12345,
         discount:20,
-        imageUri:'/images/games/dota_2.jpg'
+        imageUri:'/images/games/dota-2.jpg'
     },
     {
         id:3,
         name:'Forza horizon 5',
-        price:987655,
+        value:4567,
         discount:15,
-        imageUri:'/images/games/forza_horizon_5.jpg'
+        imageUri:'/images/games/forza-horizon-5.jpg'
+    },
+    {
+        id:4,
+        name:'Forza horizon 4',
+        value:3456,
+        discount:15,
+        imageUri:'/images/games/forza-horizon-5.jpg'
     }
 ]
   
