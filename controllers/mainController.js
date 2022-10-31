984fb245f930868fdfddd7f253f37e29ea9f54a9
Francisco Nahuel Zelaya <nahuelmassa93@gmail.com>
2022-10-31 01:30:44 -0300

    ejs en main y product models add

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,9 @@
 const path = require("path");
+let products = require('../models/product')
 
 const controlador = {
   index: (req, res) => {
-    res.render('main.ejs');
+    res.render('main.ejs', {products:products});
   },
   about: (req, res) => {
     res.sendFile(path.resolve(__dirname, "../views/about.html"));
--- models/product.js
+++ models/product.js
@@ -5,7 +5,9 @@ const products = [
         value:12345,
         consoleType:'PS4',
         discount:25,
-        imageUri:'/images/games/need_for_speed_heat.jpg'
+        imageUri:'/images/games/need_for_speed_heat.jpg',
+        section: 'none',
+        tag: '/nfsheat'
     },
     {
         id:2,
@@ -13,7 +15,9 @@ const products = [
         value:12345,
         consoleType:'PS5',
         discount:20,
-        imageUri:'/images/games/dota-2.jpg'
+        imageUri:'/images/games/dota-2.jpg',
+        section: 'none',
+        tag: '/dota2'
     },
     {
         id:3,
@@ -21,16 +25,130 @@ const products = [
         value:4567,
         consoleType:'XBOX ONE',
         discount:15,
-        imageUri:'/images/games/forza-horizon-5.jpg'
+        imageUri:'/images/games/forza-horizon-5.jpg',
+        section: 'none',
+        tag: '/forza5'
     },
     {
         id:4,
         name:'Terraria',
+        value:11.98,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/main-cards/terraria.jpg',
+        section: 'sellers',
+        tag: '/terraria'
+    },
+    {
+        id:5,
+        name:'Cyberpunk 2077',
+        value:9.99,
+        consoleType:'XBOX',
+        discount:15,
+        imageUri:'/images/main-cards/cyberpunk.jpeg',
+        section: 'sellers',
+        tag: '/cyberpunk'
+    },
+    {
+        id:6,
+        name:'Fortnite - Season 4',
+        value:3.99,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/main-cards/fortnite.jpeg',
+        section: 'sellers',
+        tag: '/fortnite'
+    },
+    {
+        id:7,
+        name:'PlayStation Card',
+        value:9.99,
+        consoleType:'PS5',
+        discount:15,
+        imageUri:'/images/main-cards/playstation-store.jpeg',
+        section: 'sellers',
+        tag: '/pscard'
+    },
+    {
+        id:8,
+        name:'ARK Survival Evolved',
+        value:7.50,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/main-cards/ark-survival.jpeg',
+        section: 'recommended',
+        tag: '/ark'
+    },
+    {
+        id:9,
+        name:'Counter Strike',
+        value:3.99,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/main-cards/cs-anthology.jpg',
+        section: 'recommended',
+        tag: '/csanthology'
+    },
+    {
+        id:10,
+        name:'Doom Eternal - Deluxe',
+        value:3.99,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/main-cards/doom-eternal.jpeg',
+        section: 'recommended',
+        tag: '/doom-eternal'
+    },
+    {
+        id:11,
+        name:'COD - Infinite Warfare',
+        value:3.99,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/main-cards/cod-infinite.jpg',
+        section: 'recommended',
+        tag: '/cod-iw'
+    },
+    {
+        id:12,
+        name:'Captain Tsubasa',
         value:3456,
         consoleType:'PC',
         discount:15,
-        imageUri:'/images/games/terraria2.avif'
+        imageUri:'/images/main-cards/captain-tsubasa.jpeg',
+        section: 'offer',
+        tag: '/captain-tsubasa'
     },
+    {
+        id:13,
+        name:'Dragon Ball Z Kakarot',
+        value:3456,
+        consoleType:'XBOX',
+        discount:15,
+        imageUri:'/images/main-cards/dbz-kakarot.jpeg',
+        section: 'offer',
+        tag: '/dbz-kakarot'
+    },
+    {
+        id:14,
+        name:'FIFA 23',
+        value:3456,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/main-cards/fifa23.jpg',
+        section: 'offer',
+        tag: '/fifa23'
+    },
+    {
+        id:15,
+        name:'Monster Hunter World',
+        value:6.99,
+        consoleType:'PC',
+        discount:15,
+        imageUri:'/images/games/terraria2.avif',
+        section: 'offer',
+        tag: '/mhw'
+    }
  
 ]
   
