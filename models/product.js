769015072bd24dc541acf5c65a159f1c9a4b51c0
Gustavo Avella <gusavella1984@gmail.com>
2022-10-30 20:08:22 -0500

    Agrego tipo de consola para renderizacion del producto en el carrito

--- models/product.js
+++ models/product.js
@@ -3,6 +3,7 @@ const products = [
         id:1,
         name:'Need for speed heat',
         value:12345,
+        consoleType:'PS4',
         discount:25,
         imageUri:'/images/games/need_for_speed_heat.jpg'
     },
@@ -10,6 +11,7 @@ const products = [
         id:2,
         name:'Dota 2',
         value:12345,
+        consoleType:'PS5',
         discount:20,
         imageUri:'/images/games/dota-2.jpg'
     },
@@ -17,6 +19,7 @@ const products = [
         id:3,
         name:'Forza horizon 5',
         value:4567,
+        consoleType:'XBOX ONE',
         discount:15,
         imageUri:'/images/games/forza-horizon-5.jpg'
     },
@@ -24,9 +27,11 @@ const products = [
         id:4,
         name:'Forza horizon 4',
         value:3456,
+        consoleType:'PC',
         discount:15,
         imageUri:'/images/games/forza-horizon-5.jpg'
-    }
+    },
+ 
 ]
   
   module.exports = products;
