228b6f6aba70f899a89cc9bb8b4b85182eba470c
Gustavo Avella <gusavella1984@gmail.com>
2022-11-19 08:41:45 -0500

    Añado nuevas dependencias y agrego comparacion de contraseña en login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const bcrypt= require("bcryptjs")
 
 const usersFilePath = path.join(__dirname, "../models/users.json");
 let users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
@@ -9,12 +10,16 @@ const controller = {
       res.render("login/login.ejs",{tittle:'Login'});
     },
     checkLogin: (req,res) => {
-      let userLoged = req.body
+      let userLoged = {...req.body}
+      console.log(userLoged)
+      let encryptPass=bcrypt.hashSync(`${userLoged.password}`,10)
+      console.log(encryptPass)
       users.forEach((user) => {
-        if(user.email == userLoged.email){
+        if(user.email == userLoged.email && bcrypt.compareSync(userLoged.password,user.password)){
           
+           res.send('usuario encontrado y validado el password..excelente')
         } else {
-          
+          res.render("login/login.ejs",{tittle:'Login'});
         }
       })
     }
