ac1eb1e986400bd88ceb7a92735854b43f4df1fa
NGiancristiano <ngiancristiano1@gmail.com>
2022-12-05 22:17:14 +0900

    agregando authMiddleware al carrito

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware')
 const cartController =require('../controllers/cartController');
 
-router.get('/',cartController.cart)
+router.get('/',authMiddleware,cartController.cart)
 
-router.post('/add/:id',cartController.addToCart)
+router.post('/add/:id',authMiddleware,cartController.addToCart)
 
-router.delete('/:id',cartController.delete)
+router.delete('/:id',authMiddleware,cartController.delete)
 
 module.exports = router;
