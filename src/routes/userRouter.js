f07302ca4b96c9327ce4839be0fe1bb52c81f268
Gustavo Avella <gusavella1984@gmail.com>
2022-12-23 18:43:12 -0500

    commit 20221223

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,8 +1,32 @@
 let express = require('express');
 let router = express.Router();
+const multer = require('multer');
 const userController =require('../controllers/userController');
+const validateLoginmiddleware= require('../middlewares/validateLoginMiddleware')
+const guestMiddleware = require('../middlewares/guestMiddleware')
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, path.join(__dirname,'../../public/images/users'))
+    },
+    filename: function (req, file, cb) {
+    cb(null,   Date.now()+path.extname(file.originalname))
+    }
+   })
+   let upload = multer({ storage: storage })
 
 router.get('/detail/:id',userController.detail)
+//Login
+router.get('/login',guestMiddleware,userController.login)
 
+router.post('/',validateLoginmiddleware,userController.loginProcess)
+
+router.get('/logout',userController.logout)
+//Login
+//REGISTER
+router.get ("/register", guestMiddleware, userController.register);
+
+router.post('/register', upload.single('image'), userController.createUser);
+//REGISTER
 
 module.exports = router;
