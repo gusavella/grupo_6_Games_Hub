cad5ef65bacbfe4c20bb336a6e1ff6f4fe8d13d4
Francisco Nahuel Zelaya <nahuelmassa93@gmail.com>
2023-01-07 22:03:29 -0300

    Categories Nahuel

--- src/routes/categoryRouter.js
+++ src/routes/categoryRouter.js
@@ -2,6 +2,9 @@ let express = require('express');
 let router = express.Router();
 const categoryController =require('../controllers/categoryController');
 
-   router.get('/all',categoryController.all)
+   router.get('/all',categoryController.all);
+   router.post('/add',categoryController.create);
+   router.put('/:id/edit',categoryController.update);
+   router.delete('/:id',categoryController.delete);
 
 module.exports = router;
