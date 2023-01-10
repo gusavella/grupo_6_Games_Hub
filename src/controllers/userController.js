34ddfaa081a9231091567e748dab9bff88166926
NGiancristiano <ngiancristiano1@gmail.com>
2023-01-10 09:10:14 +0900

    guardando parte del crud user

---
+++ src/controllers/userController.js
@@ -0,0 +1,107 @@
+const fs = require("fs");
+const path = require("path");
+const bcrypt= require("bcryptjs")
+const {	validationResult, body} = require('express-validator');
+const User = require("../models/Users")
+
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+
+const productsFilePath = path.join(__dirname, "../database/products.json");
+let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+
+
+const controller = {
+    all: async (req, res) => {
+      try{
+        let users= await db.User.findAll({include:["role"]})
+        res.json(users);
+      }
+      catch(e){
+        console.log(e)
+      }
+      
+    },
+    detail: (req, res) => {
+          let user=User.findByPk(req.params.id)
+          console.log(user)
+        res.render("users/userDetail.ejs", { tittle: "User Detail",user:user });
+    },
+    login: (req, res) => {
+      console.log('cookies:',req.cookies)
+      res.render("users/login.ejs",{tittle:'Login'});
+    },
+    loginProcess: (req,res) => {
+      console.log(req.body)
+      const resultValidation = validationResult(req);
+
+		if (resultValidation.errors.length > 0) {
+			return res.render('users/login.ejs', {tittle:'Login',
+				errors: resultValidation.mapped(),
+				oldData: req.body
+			});
+		}
+      let userToLogin = User.findByField('email',req.body.email)
+
+      if(userToLogin){
+        let isOkPassword = bcrypt.compareSync(req.body.password,userToLogin.password)
+        if(isOkPassword){
+          delete userToLogin.password
+          req.session.userLogged=userToLogin
+          if(req.body.rememberCheck){
+            res.cookie('userEmail',`${userToLogin.email}`,{maxAge:1000*60*10})
+          }
+         
+          console.log(req.session)
+            return res.redirect(`/users/detail/${userToLogin.id}`)
+        }
+        return res.render('users/login.ejs',{ tittle:'Login',
+                                  errors:{
+                                    password:{
+                                      msg:'Credenciales invalidas - password'
+                                    }
+                                  }
+                  })
+      }
+
+      return res.render('users/login.ejs',{ tittle:'Login',
+                                  errors:{
+                                    email:{
+                                      msg:'Credenciales invalidas - email '
+                                    }
+                                  }
+                  })
+
+    },
+    logout: (req,res) => {
+      res.clearCookie('userEmail')
+      req.session.destroy()
+      return res.redirect('/')
+    },
+    register : (req, res) => {
+      res.render ("register/register.ejs",{tittle:'Register'});
+  },
+  createUser: async (req, res) => {
+      let newUser = req.body
+      try{
+        let createdUser = await db.User.create({
+          names:newUser.names,
+          surnames:newUser.surnames,
+          email:newUser.email,
+          password:bcrypt.hashSync(newUser.password,10),
+          address:newUser.address,
+          phone:newUser.phone,
+          image:req.file?'/images/users/'+ req.file.filename:'/images/defaultImage.png'
+      })
+      res.redirect('/')
+      }
+
+      catch(e){console.log(e)}
+  },
+  profile: (req, res) => {
+      res.render('users/userDetail', {tittle:'Games Hub'})
+  }
+};
+
+module.exports = controller;
