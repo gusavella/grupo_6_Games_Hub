c2a92d2d9f89d924e4a28dd8b374c296e5d70260
FelipeAMS <xfoarmks@gmail.com>
2023-02-01 00:05:34 -0400

    Agregando validacion de registro desde el front end y arreglando detalles de css

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -1,11 +1,11 @@
 const { body } = require('express-validator');
 module.exports = [
-    body('names').notEmpty().withMessage('Debes escribir un nombre'),
-    body('surnames').notEmpty().withMessage('Debes escribir un apellido'),
-    body('password').notEmpty().withMessage('Debes escribir una contraseña'),
-    body('phone').notEmpty().withMessage('Debes escribir un número de teléfono'),
+    body('names').notEmpty().withMessage('Debes ingresar un nombre'),
+    body('surnames').notEmpty().withMessage('Debes ingresar un apellido'),
+    body('password').notEmpty().withMessage('Debes ingresar una contraseña'),
+    body('phone').notEmpty().withMessage('Debes ingresar un número de teléfono'),
     body('email')
-        .notEmpty().withMessage('Debes escribir un email válido').bail()
-        .isEmail().withMessage('Debes escribir un formato de correo válido'),
-    body('address').notEmpty().withMessage('Debes añadir tú dirección'),
+        .notEmpty().withMessage('Debes ingresar un email').bail()
+        .isEmail().withMessage('Debes ingresar un formato de correo válido'),
+    body('address').notEmpty().withMessage('Debes ingresar tú dirección'),
 ]
