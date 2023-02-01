93facb5e4bec0fe80da1fe18acf30234e49c6b38
Gustavo Avella <gusavella1984@gmail.com>
2023-01-31 21:11:23 -0500

    arreglos de estilos

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -4,23 +4,22 @@ document.querySelectorAll(".carousel").forEach(carousel => {
         return `<span class="carousel-button"></span>`;
     });
     
-    carousel.insertAdjacentHTML('beforeend', `
-    <div class="carousel-nav">
-    ${button_html.join("")}
-    </div>
-    `);
+    carousel.insertAdjacentHTML('beforeend', `<div class="carousel-nav">
+                                                ${button_html.join("")}
+                                               </div>`);
 
     const buttons = carousel.querySelectorAll('.carousel-button')
 
     buttons.forEach((button, i) => {
-        button.addEventListener('click', () => {
-            items.forEach(item => item.classList.remove('carousel-item-selected'));
-            buttons.forEach(button => button.classList.remove('carousel-buton-selected'));
+                                    button.addEventListener('click', () => {
+                                                                            items.forEach(item => item.classList.remove('carousel-item-selected'));
+                                                                            buttons.forEach(button => button.classList.remove('carousel-button-selected'));
+                                                                            items[i].classList.add('carousel-item-selected');
+                                                                            button.classList.add('carousel-button-selected');
+                                                                        })
 
-            items[i].classList.add('carousel-item-selected');
-            button.classList.add('carousel-button-selected');
-        })
     })
+
     items[0].classList.add('carousel-item-selected');
     buttons[0].classList.add('carousel-button-selected');
 });
