edd7b33677e6914f6a2a317b009677ca264550c7
Gustavo Avella <gusavella1984@gmail.com>
2023-02-05 17:46:35 -0500

    Modificacion de footer

--- Dashboard_react/src/components/Footer.js
+++ Dashboard_react/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer(){
 			<footer className="sticky-footer bg-white">
 				<div className="container my-auto">
 					<div className="copyright text-center my-auto">
-						<span>Copyright &copy; Dashboard 2021</span>
+						<span>Copyright &copy; GamesHub 2023</span>
 					</div>
 				</div>
 			</footer>
