49475fb9f1106e27c25a786f3dc9cea1c6c3b3ce
Gustavo Avella <gusavella1984@gmail.com>
2023-02-11 06:01:54 -0500

    Elimino data innecesaria en tabla

--- Dashboard_react/src/components/Chart.js
+++ Dashboard_react/src/components/Chart.js
@@ -1,24 +1,6 @@
 import React from 'react';
 import ChartRow from './ChartRow';
 
-let tableRowsData = [
-    {
-        Title: 'Billy Elliot ',
-        Length: '123',
-        Rating: '5',
-        Categories: ['Drama','Comedia'],
-        Awards: 2
-    },
-    {
-        Title: 'Alicia en el país de las maravillas',
-        Length: '142',
-        Rating: '4.8',
-        Categories: ['Drama','Acción','Comedia'],
-        Awards: 3
-    },
-    
-]
-
 
 function  Chart ({infoUsers}){
     if (infoUsers!== undefined){
